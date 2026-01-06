0c466cea1f82d284d23350285813fb6a2312d5b8
keer01 <136733990+keer01@users.noreply.github.com>
2026-01-06 12:50:39 +0530

    Add files via upload
    Employee Management done using servlet and JSP.

--- EmployeeManagement/src/main/java/com/emp/dao/EmployeeDAO.java
+++ EmployeeManagement/src/main/java/com/emp/dao/EmployeeDAO.java
@@ -1,17 +1,49 @@
 package com.emp.dao;
-
-
-
+ 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
 import java.util.List;
-import com.emp.entity.Employee;
  
-public interface EmployeeDAO {
+import com.emp.model.Employee;
+import com.emp.util.DBConnection;
  
-    Employee addEmployee(Employee emp);
+public class EmployeeDAO {
  
-    Employee updateEmployee(Employee emp);
+    public void saveEmployee(Employee emp) {
+        try {
+            Connection con = DBConnection.getConnection();
+            String sql = "INSERT INTO employee(name,email,salary) VALUES(?,?,?)";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, emp.getName());
+            ps.setString(2, emp.getEmail());
+            ps.setDouble(3, emp.getSalary());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
  
-    List<Employee> deleteEmployee(int id);
+    public List<Employee> getAllEmployees() {
+        List<Employee> list = new ArrayList<>();
+        try {
+            Connection con = DBConnection.getConnection();
+            PreparedStatement ps = con.prepareStatement("SELECT * FROM employee");
+            ResultSet rs = ps.executeQuery();
  
-    List<Employee> getAllEmployees();
+            while (rs.next()) {
+                Employee e = new Employee();
+                e.setId(rs.getInt("id"));
+                e.setName(rs.getString("name"));
+                e.setEmail(rs.getString("email"));
+                e.setSalary(rs.getDouble("salary"));
+                list.add(e);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return list;
+    }
 }
+ 
